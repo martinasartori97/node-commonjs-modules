af160ca1354924ed1fb7a81204d4c9d1344f0ee9
martinasartori97 <martinasartori9@gmail.com>
2024-10-29 15:05:09 +0100

    edit js

--- names.js
+++ names.js
@@ -1,7 +1,9 @@
+const nomeIntero = ('martina', 'sartori');
 function fullName(firstName, lastName) {
     const risultato = `${firstName} ${lastName}`;
     return risultato;
 }
-console.log(fullName('martina', 'sartori'));
+
+console.log(nomeIntero);
 
 module.exports = fullName
